fc1ee1f3c987b450b4dd9179821c94e01efe654e
Aniruddha <aniruddhavikharankarfb@gmail.com>
2022-11-27 12:13:02 +0530

    ani changes navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,33 @@
-import React from "react";
+import { useState } from "react"
+import "../styles/navbar.css"
 
-const Navbar = () => {
-  return <div>Navbar</div>;
-};
+export default function Navbar() {
+  const [isNavExpanded, setIsNavExpanded] = useState(false)
 
-export default Navbar;
+  return (
+    <nav className="navigation">
+      <a href="/" className="brand-name">
+        QuickLance
+      </a>
+      <button
+        className="hamburger"
+        onClick={() => {
+          setIsNavExpanded(!isNavExpanded)
+        }}
+      >
+        {/* hamburger svg code... */}
+      </button>
+      <div
+        className={
+          isNavExpanded ? "navigation-menu expanded" : "navigation-menu"
+        }
+      >
+        <ul className={isNavExpanded ? "ListStyle" : ""}>
+          <li>
+            <a href="/home">Connect Wallet</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+}
