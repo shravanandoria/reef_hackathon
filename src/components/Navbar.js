6abfa80c24bb8bb943224fc067a1efe0735e3790
Aniruddha <aniruddhavikharankarfb@gmail.com>
2022-11-27 13:26:51 +0530

    changes by ani

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,33 +1,36 @@
 import { useState } from "react"
 import "../styles/navbar.css"
+import Uik from "@reef-defi/ui-kit"
+
 
 export default function Navbar() {
   const [isNavExpanded, setIsNavExpanded] = useState(false)
+  const [firstTab, setFirstTab] = useState("First")
+  const [secondTab, setSecondTab] = useState("tokens")
 
   return (
-    <nav className="navigation">
-      <a href="/" className="brand-name">
-        QuickLance
-      </a>
-      <button
-        className="hamburger"
-        onClick={() => {
-          setIsNavExpanded(!isNavExpanded)
-        }}
-      >
-        {/* hamburger svg code... */}
-      </button>
-      <div
-        className={
-          isNavExpanded ? "navigation-menu expanded" : "navigation-menu"
-        }
-      >
-        <ul className={isNavExpanded ? "ListStyle" : ""}>
-          <li>
-            <a href="/home">Connect Wallet</a>
-          </li>
-        </ul>
-      </div>
-    </nav>
+    <>
+      <nav className="navigation">
+        <a href="/" className="brand-name">
+          QuickLance
+        </a>
+        <button
+          className="hamburger"
+          onClick={() => {
+            setIsNavExpanded(!isNavExpanded)
+          }} J
+        >
+        </button>
+        <div
+          className={
+            isNavExpanded ? "navigation-menu expanded" : "navigation-menu"
+          }
+        >
+          <ul className={isNavExpanded ? "ListStyle" : ""}>
+            <Uik.Button text='Connect Wallet' />
+          </ul>
+        </div>
+      </nav>
+    </>
   );
 }
--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,8 +1,24 @@
 import React from 'react'
+import "../styles/homepage.css"
+import HeroSvg from "../assets/hero.svg"
+import Uik from '@reef-defi/ui-kit'
 
 const Homepage = () => {
   return (
-    <div>Homepage</div>
+    <div className='heroContainer'>
+      <div>
+        <Uik.Text text="Find The Best Work For You!" type="headline" />
+        <Uik.Text text="Request | Work | Earn" type="headline" />
+        <Uik.Text className='normalText' text="Choose your direction and let's start!!" type="light" />
+        <div className='ButtonContainer'>
+          <Uik.Button className='btn1' text='Quick Project' fill />
+          <Uik.Button text='Get Started' success />
+        </div>
+      </div>
+      <div>
+        <img src={HeroSvg} />
+      </div>
+    </div>
   )
 }
 
