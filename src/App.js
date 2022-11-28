001eac4822d3cf5f12e8c4dfa9745075eb64e1b1
Aniruddha <aniruddhavikharankarfb@gmail.com>
2022-11-28 14:58:04 +0530

    latest changes

--- src/App.js
+++ src/App.js
@@ -12,56 +12,10 @@ import CreateProject from "./pages/CreateProject";
 import Freelancers from "./pages/Freelancers";
 import Footer from "./components/Footer";
 
-const FactoryAbi = ProjectFactory.abi;
-const factoryContractAddress = ProjectFactory.address;
-
-const URL = "wss://rpc-testnet.reefscan.com/ws";
+// const FactoryAbi = ProjectFactory.abi;
+// const factoryContractAddress = ProjectFactory.address;
 
 function App() {
-  const [msgVal, setMsgVal] = useState("");
-  const [msg, setMsg] = useState("");
-  const [signer, setSigner] = useState();
-  const [isWalletConnected, setWalletConnected] = useState(false);
-
-  const checkExtension = async () => {
-    let allInjected = await web3Enable("Reef");
-
-    if (allInjected.length === 0) {
-      return false;
-    }
-
-    let injected;
-    if (allInjected[0] && allInjected[0].signer) {
-      injected = allInjected[0].signer;
-    }
-
-    const evmProvider = new Provider({
-      provider: new WsProvider(URL),
-    });
-
-    evmProvider.api.on("ready", async () => {
-      const allAccounts = await web3Accounts();
-
-      allAccounts[0] && allAccounts[0].address && setWalletConnected(true);
-
-      console.log(allAccounts);
-
-      const wallet = new Signer(evmProvider, allAccounts[0].address, injected);
-
-      // Claim default account
-      if (!(await wallet.isClaimed())) {
-        console.log(
-          "No claimed EVM account found -> claimed default EVM account: ",
-          await wallet.getAddress()
-        );
-        await wallet.claimDefaultAccount();
-      }
-
-      console.log({ wallet });
-
-      setSigner(wallet);
-    });
-  };
 
   // const checkSigner = async () => {
   //   if (!signer) {
@@ -96,10 +50,10 @@ function App() {
   return (
     <div>
       <Navbar />
-      {/* <Homepage /> */}
+      <Homepage />
       {/* <Freelancers /> */}
       {/* <EditProfile /> */}
-      <CreateProject />
+      {/* <CreateProject /> */}
       <Footer />
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -10,3 +10,29 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById("root")
 );
+
+
+// import React from "react";
+// import ReactDOM from "react-dom";
+// import { BrowserRouter, Routes, Route } from "react-router-dom";
+// import "./styles/index.css";
+// import App from "./App";
+// import "bootstrap/dist/css/bootstrap.min.css";
+// import Homepage from "./pages/Homepage";
+// import CreateProject from "./pages/CreateProject";
+// import Freelancers from "./pages/Freelancers";
+
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <BrowserRouter>
+//       <Routes>
+//         <Route path="/" element={<App />}>
+//           <Route index element={<Homepage />} />
+//           <Route path="Freelancers" element={<Freelancers />} />
+//           <Route path="CreateProject" element={<CreateProject />} />
+//         </Route>
+//       </Routes>
+//     </BrowserRouter>
+//   </React.StrictMode>,
+//   document.getElementById("root")
+// );
